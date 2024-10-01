b6f3a5fcc0ff01339705e516dda066c7213e3a73
Randa_Lucie <lucie.bories01@gmail.com>
2024-10-01 11:55:49 +0200

    calcul des couts - Fin sceance tp2

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -15,25 +15,43 @@ class Local(models.Model):
     nom_local = models.CharField(max_length=100)
     ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
     surface_local = models.IntegerField()
+    
     def __str__(self):
-        return f"{self.nom_local}"
+        return f"{self.nom_local} {self.ville} {self.surface_local}m2"
+        
+    def costs(self):
+    	return (self.surface_local) * (self.ville.prix_m2)
 
 class Machine(models.Model):
     nom_machine = models.CharField(max_length=100)
     prix_machine = models.IntegerField()
     n_serie_machine = models.IntegerField()
+    
     def __str__(self):
         return f"{self.nom_machine}{self.n_serie_machine}"
+        
+    def costs(self):
+    	return  self.prix_machine
 
 class Objet(models.Model):
     nom_objet = models.CharField(max_length=100)
     prix_objet = models.IntegerField()
     def __str__(self):
         return f"{self.nom_objet}"
+      
+    def costs(self):
+    	return self.prix_objet 
+       
 
 class Usine(Local):
     machines = models.ManyToManyField(Machine)
 
+    def costs(self):
+        prix_machines = 0
+        for machine in self.machines.all():
+            prix_machines = prix_machines + machine.prix_machine
+        return prix_machines
+    	
 
 class SiegeSocial(Local):
     pass
@@ -46,14 +64,22 @@ class Ressource(Objet):
 class QuantiteRessource(models.Model):
     ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
     quantite = models.IntegerField()
+    
+    def costs(self):     
+        return self.ressource.prix_objet * self.quantite
+
+    
 
 
 class Stock(models.Model):
     objet = models.ForeignKey(Objet, on_delete=models.PROTECT)
     nombre = models.IntegerField()
     usine = models.ForeignKey(Usine, on_delete=models.PROTECT)
+    
     def __str__(self):
         return f"{self.objet}"
+        
+    
 
 class Etape(models.Model):
     nom_etape = models.CharField(max_length=100)
--- crayon/high_level/tests.py
+++ crayon/high_level/tests.py
@@ -1,1 +1,14 @@
 # Create your tests here.
+from django.test import TestCase
+
+from .models import Machine
+
+
+class MachineModelTests(TestCase):
+    def test_machine_creation(self):
+        self.assertEqual(Machine.objects.count(), 0)
+        Machine.objects.create(nom_machine="scie",
+                               prix_machine=1_000,
+                               n_serie_machine=44365)
+        self.assertEqual(Machine.objects.count(), 2)
+
