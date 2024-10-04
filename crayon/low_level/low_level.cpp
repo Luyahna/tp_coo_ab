72482de04f802c4c553a3bbe2a33f554f2cfb1fc
Randa_Lucie <lucie.bories01@gmail.com>
2024-10-04 17:42:05 +0200

    fin seance 4: ajout classe local et ajout des 3 constructeurs

--- crayon/low_level/low_level.cpp
+++ crayon/low_level/low_level.cpp
@@ -1,7 +1,10 @@
+
 #include<string>
 #include<iostream>
 #include<cpr/cpr.h>
 #include<nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 using namespace std;
 
@@ -12,13 +15,70 @@ private :
 	float prix_m2;
 
 public :
-	Ville( string nom_ville , int code_postal ,float prix_m2): nom_ville(nom_ville),           	  code_postal(code_postal), prix_m2(prix_m2) {}
+	Ville( string nom_ville , int code_postal ,float prix_m2): nom_ville(nom_ville), code_postal(code_postal), prix_m2(prix_m2) {}
 	
+        Ville(const json& data): nom_ville(data["nom_ville"].get<string>()),
+        code_postal(data["code_postal"].get<int>()),
+        prix_m2(data["prix_m2"].get<float>()) {}
+        
+	Ville(int id) 
+	{
+		cpr::Response r = cpr::Get(cpr::Url{"http://127.0.0.1:8000/villes/" + to_string(id) + "/"});
+
+		json j = json::parse(r.text);
+		nom_ville = j["nom_ville"].get<string>();
+		code_postal = j["code_postal"].get<int>();
+		prix_m2 = j["prix_m2"].get<float>();
+    	}
+    	
+	void afficher() const 
+	{
+		cout << "Nom de la ville: " << nom_ville << endl;
+		cout << "Code postal: " << code_postal << endl;
+		cout << "Prix au m2: " << prix_m2 << " EUR" << endl;
+        }
+        
+};
+
+class Local {
+private :
+	string nom_local;
+	unique_ptr<Ville> p_ville;
+	int surface_local;
+	int ville; 
 	
-	void afficher() const {
-        cout << "Nom de la ville: " << nom_ville << endl;
-        cout << "Code postal: " << code_postal << endl;
-        cout << "Prix au m²: " << prix_m2 << " EUR" << endl;
+
+public :
+	Local( string nom_local_, int surface_local_, int ville_ ) : nom_local(nom_local_), ville(ville_), surface_local(surface_local_) {
+	    p_ville = make_unique<Ville>(ville);
+	}
+	
+        Local(const json& data): nom_local(data["nom_local"]),
+        surface_local(data["surface_local"]),
+        ville(data["ville"]) {
+        	p_ville = make_unique<Ville>(ville);
+        }
+        
+	Local(int id) 
+	{
+		cpr::Response r = cpr::Get(cpr::Url{"http://127.0.0.1:8000/local/" + to_string(id) + "/"});
+
+		json j = json::parse(r.text);
+		nom_local = j["nom_local"];
+		surface_local = j["surface_local"];
+		ville = j["ville"];
+		p_ville = make_unique<Ville>(ville);
+    	}
+    	
+
+	void afficher1() const {
+	
+		cout << "Nom du local: " << nom_local << endl;
+		cout << "Surface du local: " << surface_local << " m2" << endl;
+		cout << "ID de la ville: " << ville << endl;
+		
+		//p_ville->afficher(); // Affiche les informations de la ville
+	
         }
 };
 
@@ -26,15 +86,20 @@ public :
 
 int main()
 {
-	Ville v("toulouse",31400,5000);
-	//v.afficher();
 	
-	cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"});
-	r.status_code;                  // 200
-	r.header["content-type"];       // application/json; charset=utf-8
-	r.text;                         // JSON text string
+	//r.status_code;                  // 200
+	//r.header["content-type"];       // application/json; charset=utf-8
+	//r.text;				// JSON text string
 
-	cout<<r.text<<endl<<r.status_code<<endl;
+	const auto v = Ville{1};
+	
+	v.afficher();
+	
+	const auto l = Local{4};
+	l.afficher1();
+	
+	//cout<<r.text<<endl<<r.status_code<<endl;
+	
 	return 0;
 }
 
